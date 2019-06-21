142fb34eba96058abce05db9dee2a9a2609efbd1
Lihzahrd <47698047+Lihzahrd@users.noreply.github.com>
2019-06-21 10:11:47 -0700

    Update modboxcurrent.js

--- BeepBox_files/modboxcurrent.js
+++ BeepBox_files/modboxcurrent.js
@@ -389,7 +389,7 @@ var beepbox;
     Config._drumWaves =          [null, null, null, null, null, null, null, null, null, null];
     Config.pwmwaveValues = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95];
 	Config.pwmwaveNames = [];
-	for (let i = 0; i < Config.pwmwaveValues.length; i++) {
+	for (let i = 1; i < Config.pwmwaveValues.length; i++) {
 		Config.pwmwaveNames.push(Config.pwmwaveValues[i] + "%");
 	}
     Config.pwmwaveVolumes = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0];
